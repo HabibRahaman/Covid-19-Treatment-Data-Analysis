2096abe7e71c02569ae05fd89942cce5ad2dc50e
HabibRahaman <habibcse335@gmail.com>
2020-08-27 12:57:58 +0600

    Symptom DB Migration

---
+++ database/migrations/2020_08_27_064946_create_symptoms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSymptomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('symptoms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->text('details')->nullable();
+            $table->integer('priority')->default('0');
+            $table->integer('risk_level')->default('1');
+            $table->boolean('show')->default('1');
+            $table->boolean('status')->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('symptoms');
+    }
+}
