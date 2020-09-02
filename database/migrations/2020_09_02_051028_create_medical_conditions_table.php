2a8905826cac27f7e5866769b8dce2e23300ff46
HabibRahaman <habibcse335@gmail.com>
2020-09-02 11:19:08 +0600

    Medical Condition DB Migration

---
+++ database/migrations/2020_09_02_051028_create_medical_conditions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedicalConditionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medical_conditions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->text('details')->nullable();
+            $table->text('instructions')->nullable();
+            $table->integer('risk_level')->default('1');
+            $table->boolean('show')->default('1');
+            $table->boolean('status')->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medical_conditions');
+    }
+}
