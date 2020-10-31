8f723440408d08e14e52b4179aa061a25e30ebf4
HabibRahaman <habibcse335@gmail.com>
2020-10-31 23:50:35 +0600

    Wizard validation update

--- public/web/js/index.js
+++ public/web/js/index.js
@@ -26,14 +26,14 @@ $(document).ready(function () {
         var curStep = $(this).closest(".setup-content"),
             curStepBtn = curStep.attr("id"),
             nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            curInputs = curStep.find("input[type='text'],input[type='email'],input[type='radio'],input[type='checkbox'],select"),
             isValid = true;
 
-        $(".form-group").removeClass("has-error");
+        $(".form-control, .custom-select, .form-inline").removeClass("has-error");
         for(var i=0; i<curInputs.length; i++){
             if (!curInputs[i].validity.valid){
                 isValid = false;
-                $(curInputs[i]).closest(".form-group").addClass("has-error");
+                $(curInputs[i]).closest(".form-control, .custom-select, .form-inline").addClass("has-error");
             }
         }
 
