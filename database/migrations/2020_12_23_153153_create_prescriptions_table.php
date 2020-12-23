f94c89a3eeaecc7dc16fe18e945f933d1d37ebdc
HabibRahaman <habibcse335@gmail.com>
2020-12-23 21:38:09 +0600

    Prescription DB migration

---
+++ database/migrations/2020_12_23_153153_create_prescriptions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrescriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prescriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('patient_id')->unsigned();
+            $table->text('details')->nullable();
+            $table->timestamps();
+
+            $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prescriptions');
+    }
+}
